979f1b136e06fe0e5f7d3f4a8fcb7b4667e5f382
dharmika1234 <162439534+dharmika1234@users.noreply.github.com>
2025-09-08 15:36:02 +0530

    ReverseOppoLShape

---
+++ ReverseOppoLShape.java
@@ -0,0 +1,15 @@
+public class ReverseOppoLShape{
+    public static void main(String[] args){
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+          if(i==n || j==n){
+              System.out.print("* ");
+            }
+          else{
+              System.out.print("  ");
+            }
+        }
+          System.out.println();
+      }
+  }
+}
